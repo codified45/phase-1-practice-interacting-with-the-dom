a6c09a3013aec71394734a6e642e2fe910fa5557
Jack Lach <jack.lach4@gmail.com>
2023-02-13 00:58:27 -0500

    increment and decrement counter buttons are working

--- js/challenge.js
+++ js/challenge.js
@@ -1,22 +1,40 @@
 
-let secondsH1 = document.getElementById('counter');
-console.log(secondsH1.textContent);
-setInterval(counterFunc, 1000);
-console.log(secondsH1);
+
 
 const init = () => {
-    
-    console.log(parseInt(secondsH1.textContent));
-    
+    let secondsH1 = document.getElementById('counter'); // can't include .textContent method because then it will be a pass-by-value
     console.log(secondsH1.textContent);
+    setInterval(counterFunc, 1000);
+    console.log(secondsH1);
 
-}
 
-function counterFunc(){
-    let counterVal = parseInt(secondsH1.textContent);
-    counterVal += 1;
-    secondsH1.textContent = counterVal;
+    document.getElementById('minus').addEventListener('click', decrementCounter);
+    document.getElementById('plus').addEventListener('click', incrementCounter);
+
+    // need to preventdefault on submit button. probably. 
+
+    //functions
+    function counterFunc(){
+        let counterVal = parseInt(secondsH1.textContent);
+        counterVal += 1;
+        secondsH1.textContent = counterVal;
+    };
+    
+    function decrementCounter(e){
+        let counterVal = parseInt(secondsH1.textContent);
+        counterVal -= 1;
+        secondsH1.textContent = counterVal;
+    };
+    
+    function incrementCounter(e){
+        let counterVal = parseInt(secondsH1.textContent);
+        counterVal += 1;
+        secondsH1.textContent = counterVal;
+    };
+
 };
+
+
 
         // function resetCounter(){ // make  button that uses this. 
         //     secondsH1.textContent = document.querySelector('#counter').textContent;
